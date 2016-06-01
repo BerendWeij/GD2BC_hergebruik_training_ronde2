65990763b91e904902732863192cee0db9c23c7b
Swan Chase <swancd@gmail.com>
2016-06-01 10:01:41 +0200

    more stuf

---
+++ Assets/Scripts/Data/Tags.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags
+{
+    public const string PLAYER = "Player";
+    public const string PLATFORMTARGET = "PlatformTarget";
+    public const string PROJECTILE = "Projectile";
+}
