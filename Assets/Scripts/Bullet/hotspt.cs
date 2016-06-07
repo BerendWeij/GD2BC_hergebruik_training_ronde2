6552c43c7b1c57418356a16f296bb2b32547e300
Icedia <daniel1995@live.nl>
2016-06-07 14:20:27 +0200

    bezig extension 2
    dvdsd

---
+++ Assets/Scripts/Bullet/hotspt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hotspt : MonoBehaviour, IDestructable
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log(coll.tag);
+        if (coll.tag == BulletTags._target)
+        {
+
+            coll.GetComponent<IDamageable>().TakeDamage(10);
+            Destruct();
+        }
+    }
+
+    public void Destruct()
+    {
+        Destroy(this.gameObject);
+    }
+}
