3a4a6c817046bcc8b46b9cf60582c6a193a61feb
Icedia <daniel1995@live.nl>
2016-06-07 12:30:50 +0200

    Tried in class
    is niet veel afgekomen :dancer:

---
+++ Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(WaitASec(4.0F));
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    IEnumerator WaitASec(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Script/Weaponswitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Weaponswitch : MonoBehaviour {
+    [SerializeField]private List<string> _Weaponlist;
+
+	// Use this for initialization
+	void Start () {
+        _Weaponlist.Add("Shotgun");
+        _Weaponlist.Add("Rifle");
+        _Weaponlist.Add("Pistol");
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Debug.Log(_Weaponlist.GetRandom());
+        }
+        
+	
+	}
+}
